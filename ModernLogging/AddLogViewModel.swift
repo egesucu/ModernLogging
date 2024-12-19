db22526c74e97fbfca47aa9846fa13efe09111d7
Ege Sucu <ege.sucu@adesso.com.tr>
2024-12-19 13:06:03 +0300

    Modified Log Adding Views with UI tests

---
+++ ModernLogging/AddLogViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddLogViewModel.swift
+//  ModernLogging
+//
+//  Created by Sucu, Ege on 7/12/24.
+//
+
+import Foundation
---
+++ ModernLoggingTests/AddLogViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  AddLogViewModelTests.swift
+//  ModernLoggingTests
+//
+//  Created by Sucu, Ege on 7/12/24.
+//
+
+import Testing
+
+struct AddLogViewModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
